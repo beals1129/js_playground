382850f91e70d5b8b68b6cfe0d996ec584a5f1fe
beals1129 <57256728+beals1129@users.noreply.github.com>
2021-02-03 23:35:21 +0900

    onclick 에 function 추가하기

--- js/add_button.js
+++ js/add_button.js
@@ -5,6 +5,7 @@ var menu = document.createElement('ul');
 var item1 = document.createElement('li');
 var item2 = document.createElement('li');
 
+var btn1 = document.getElementById('btn1')
 item1.innerHTML= "치킨";
 item2.innerHTML= "피자";
 menu.appendChild(item1);
@@ -18,3 +19,15 @@ function readInput(){
     //value inpt태그안에 들어있는것을 가져옴
 
 }
+
+// btn1.addEventListener('onclick',readInput());
+
+// btn1.onclick = readInput;
+// 함수지만 ()를 쓰지않고 onclick을 함수로 만든다. 
+
+btn1.onclick = function() {
+    console.log("click");
+    var input = document.getElementById('input1');
+    console.log(input.value);
+}
+//이런식으로 그냥 function 을 써서 가능하다. 
